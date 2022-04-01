f4cc97601a92e3fdf04a23443335d32b3eceae11
AuFish <shenchg126@gmail.com>
2022-03-31 21:58:42 -0700

    overfittable lstm model

--- ubiquant/src/lstm_predictor.py
+++ ubiquant/src/lstm_predictor.py
@@ -2,11 +2,14 @@ import torch.nn as nn
 import torch
 from torch.utils.data import DataLoader
 import os
+from tqdm import tqdm
+from typing import List
 from data_loader import AssetSerialData
 
 
 class LSTMModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob, bidirectional=True):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, fc_dim: List,
+                 dropout_prob, bidirectional=True):
         super(LSTMModel, self).__init__()
 
         # Defining the number of layers and the nodes in each layer
@@ -21,10 +24,15 @@ class LSTMModel(nn.Module):
         )
 
         # Fully connected layer
-        self.fc1 = nn.Linear(hidden_dim * (2 if bidirectional else 1), hidden_dim)
-        self.sig1 = nn.Tanh()
-        self.fc2 = nn.Linear(hidden_dim, output_dim)
-        self.sig2 = nn.Sigmoid()
+        self.fcs = nn.ModuleList()
+        fc_dim.insert(0, hidden_dim * (2 if bidirectional else 1))
+        fc_dim.append(output_dim)
+        for i in range(len(fc_dim) - 1):
+            input_dim = fc_dim[i]
+            output_dim = fc_dim[i+1]
+            self.fcs.append(nn.BatchNorm1d(input_dim))
+            self.fcs.append(nn.Linear(input_dim, output_dim))
+            self.fcs.append(nn.Tanh())
 
         self.init_hidden()
 
@@ -33,7 +41,7 @@ class LSTMModel(nn.Module):
         self.hidden = nn.Parameter(torch.zeros(dimension, 1, self.hidden_dim))
         self.cell_state = nn.Parameter(torch.zeros(dimension, 1, self.hidden_dim))
 
-    def forward(self, x: torch.tensor):
+    def forward(self, x: torch.tensor, logging=False):
         x = x.float()
         h0 = self.hidden
         c0 = self.cell_state
@@ -45,30 +53,38 @@ class LSTMModel(nn.Module):
         # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
         # so that it can fit into the fully connected layer
         out = out[:, -1, :]
+        # if logging:
+        #     print(out)
 
         # Convert the final state to our desired output shape (batch_size, output_dim)
-        out = self.fc1(out)
-        out = self.sig1(out)
-        out = self.fc2(out)
-        out = self.sig2(out)
+        for layer in self.fcs:
+            out = layer(out)
+            if logging:
+                print(layer)
+                if type(layer) == nn.Linear:
+                    print(f"linear layer weight average: {torch.mean(torch.abs(layer.weight))}")
+                    print(layer.weight)
+                    print(layer.bias)
+                print(out)
 
         return out
 
 
 def train(data_loader, model, optim, loss_fn, device):
     total_loss = 0
-    for batch in data_loader:
-        batch = batch.to(device)
-        model.zero_grad()
+    for batch in tqdm(data_loader):
+        batch = batch.to(device).float()
+        optim.zero_grad()
         x, t = batch[:, :, 1:], batch[:, -1, 0]
-        y = model(x)
+        y = model(x, logging=False)
+        # print(f"input: {x}")
+        # print(f"prediction: {y}")
+        # print(f'target: {t}')
 
-        loss = loss_fn(y, t)
+        loss = loss_fn(y, t.reshape((-1, 1)))
         loss.backward()
         optim.step()
         total_loss += loss.item()
-
-        break
 
     return total_loss
 
@@ -110,32 +126,36 @@ def save_checkpoint(model, epoch, name):
 device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 # torch.distributed.init_process_group()
 
-data_filename = "../data/combined-processed-time-series-train-length-5.pkl"
-dataset = AssetSerialData(data_filename, 5)
-data_loader = DataLoader(dataset, batch_size=64, shuffle=True)
-epoch = 50
+# data_filename = "../data/combined-processed-time-series-train-length-5.pkl"
+# dataset = AssetSerialData(data_filename, 5)
+# data_loader = DataLoader(dataset, batch_size=64, shuffle=False)
+epoch = 500
 
 test_data_filename = "../data/combined-processed-time-series-test-length-5.pkl"
 test_dataset = AssetSerialData(test_data_filename, 5)
-test_data_loader = DataLoader(test_dataset, batch_size=64)
+test_data_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-model = LSTMModel(input_dim=300, layer_dim=2, hidden_dim=32, output_dim=1, dropout_prob=0.1).to(device)
-optim = torch.optim.Adam(model.parameters(), lr=0.01)
-loss_fn = nn.L1Loss()
+model = LSTMModel(
+    input_dim=300, layer_dim=2, hidden_dim=128, fc_dim=[128, 128, 128, 64], output_dim=1, dropout_prob=0.1,
+    bidirectional=False).to(device)
+# print(model)
+optim = torch.optim.Adam(model.parameters(), lr=0.0005)
+loss_fn = nn.MSELoss()
 
 min_loss = 1000
 max_corr = -1
 for e in range(epoch):
     print(f"Training {e}th epoch")
-    loss = train(data_loader, model, optim, loss_fn, device=device)
+    # loss = train(data_loader, model, optim, loss_fn, device=device)
+    loss = train(test_data_loader, model, optim, loss_fn, device=device)
     print(f"loss: {loss}")
 
     corr = evaluate(model, test_data_loader, device=device)
     corr = corr[0][1]
     print(f"corr coef: {corr}")
-
-    if is_better_model(loss, corr, min_loss, max_corr):
-        print(f"saving model")
-        save_checkpoint(model, e, "lstm-baseline")
-        min_loss = loss
-        max_corr = corr
+    #
+    # if is_better_model(loss, corr, min_loss, max_corr):
+    #     print(f"saving model")
+    #     save_checkpoint(model, e, "lstm-baseline")
+    #     min_loss = loss
+    #     max_corr = corr
